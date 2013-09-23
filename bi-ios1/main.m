2f53a64177562675753bc094de2c37147685f1c7
Jakub Hladik <kubbing@me.com>
2013-09-23 09:23:44 +0200

    Initial Commit

---
+++ bi-ios1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bi-ios1
+//
+//  Created by Jakub Hladík on 23.09.13.
+//  Copyright (c) 2013 FlowKnight s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ bi-ios1Tests/bi_ios1Tests.m
@@ -0,0 +1,34 @@
+//
+//  bi_ios1Tests.m
+//  bi-ios1Tests
+//
+//  Created by Jakub Hladík on 23.09.13.
+//  Copyright (c) 2013 FlowKnight s.r.o. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bi_ios1Tests : XCTestCase
+
+@end
+
+@implementation bi_ios1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
