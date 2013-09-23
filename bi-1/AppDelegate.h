c0410da34b551925b54b0be25bf91a6e56b92e9d
Jakub Hladik <kubbing@me.com>
2013-09-23 07:46:02 +0200

    Initial Commit

---
+++ bi-1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bi-1
+//
+//  Created by Jakub Hladík on 23.09.13.
+//  Copyright (c) 2013 FlowKnight s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bi-1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  bi-1
+//
+//  Created by Jakub Hladík on 23.09.13.
+//  Copyright (c) 2013 FlowKnight s.r.o. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
