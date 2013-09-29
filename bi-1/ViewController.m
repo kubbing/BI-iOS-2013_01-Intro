c14344b67e8b1f8dcd2e4132df1258ec11a59b10
Jakub Hladik <kubbing@me.com>
2013-09-29 22:37:16 +0200

    final commit

--- bi-1/ViewController.m
+++ bi-1/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) UILabel *titleLabel;
+
 @end
 
 @implementation ViewController
@@ -18,12 +20,57 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIColor *color = [UIColor whiteColor];
+    self.view.backgroundColor = color;
+    
+    UILabel *titleLabel = [[UILabel alloc] init];
+    CGRect titleLabelRect = CGRectMake(8,
+                                       8 + 20,
+                                       CGRectGetWidth(self.view.bounds) - 2*8,
+                                       44);
+    titleLabel.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.4];
+    titleLabel.frame = titleLabelRect;
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.text = @"ahoj, iOS";
+    [self.view addSubview:titleLabel];
+    
+    self.titleLabel = titleLabel;
+    
+    UIImageView *imageView = [[UIImageView alloc] init];
+    titleLabelRect.origin.y += 44 + 8;
+    titleLabelRect.size.height = 120;
+    imageView.frame = titleLabelRect;
+    imageView.clipsToBounds = YES;
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.image = [UIImage imageNamed:@"img1"];
+    [self.view addSubview:imageView];
+    
+    UIButton *button = [[UIButton alloc] init];
+    button.frame = CGRectMake(8, 200, CGRectGetWidth(self.view.bounds) - 2*8, 44);
+    [button setTitle:@"Tap me!" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    
+    [button addTarget:self
+               action:@selector(buttonAction:)
+     forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:button];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (void)buttonAction:(UIButton *)sender
+{
+    self.titleLabel.text = @"button tapped";
 }
 
 @end
